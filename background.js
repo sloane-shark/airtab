964dd06ef5af988e2c21849be6901734bd195779
Matthew Ess <mess@purdue.edu>
2017-04-27 09:45:44 -0400

    Remove unnecessary functions

--- background.js
+++ background.js
@@ -1,21 +1,5 @@
 // background scripting for Airtab
 
-// function for arbitrary xhr request
-function handleReq(options) {
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = () => {
-    options.callback(xhr);
-  };
-  xhr.open(options.method, options.url, true);
-  xhr.setRequestHeader('Authorization', `Bearer ${options.token}`);
-  xhr.send(options.params);
-}
-
-// check if an object is empty
-function isEmpty(obj) {
-  return Object.keys(obj).length === 0;
-}
-
 // function to add list to registry
 function addList(list) {
   console.log(list);
@@ -30,4 +14,4 @@ function generateTabList() {
 }
 
 // handler for browser button clicks
-chrome.browserAction.onClicked.addHandler(generateTabList);
+chrome.browserAction.onClicked.addListener(generateTabList);
