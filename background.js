7ec61dfa8ed9f364b4556035bdd4cd7c72582505
Matthew Ess <mess@purdue.edu>
2017-04-27 10:12:56 -0400

    Add basic auth

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 // background scripting for Airtab
 
+// TODO: move this to an object probably
+let authToken;
+
 // function to add list to registry
 function addList(list) {
   console.log(list);
@@ -13,5 +16,35 @@ function generateTabList() {
   });
 }
 
+// auth callback
+function authCallback(token) {
+  if (chrome.runtime.lastError) {
+    chrome.browserAction.setBadge({ text: '!' });
+    chrome.browserAction.setBadgeBackgroundColor({ color: '#F00' });
+  } else {
+    console.log(token);
+    authToken = token;
+    chrome.browserAction.onClicked.addListener(generateTabList);
+  }
+}
+
+// auth function
+function auth(options = { interactive: false }) {
+  chrome.identity.getAuthToken({ interactive: options.interactive }, authCallback);
+}
+
+// silent auth
+function authSilent() {
+  auth({ interactive: false });
+}
+
+// interactive auth
+function authInteractive() {
+  auth({ interactive: true });
+}
+
 // handler for browser button clicks
-chrome.browserAction.onClicked.addListener(generateTabList);
+chrome.browserAction.onClicked.addListener(authInteractive);
+
+// attempt to authorize
+authSilent();
