d7f6fcc1512c6464cf6e8bed534cb1546f85a3f4
Matthew Ess <mess@purdue.edu>
2017-05-05 19:47:22 -0400

    Add example config

---
+++ js/example.config.js
@@ -0,0 +1,8 @@
+let config = {
+  apiKey: 'XYZ',
+  authDomain: 'myapp.firebaseapp.com',
+  databaseURL: 'https://myapp.firebaseio.com',
+  projectId: 'myapp',
+  storageBucket: 'myapp.appspot.com',
+  messagingSenderId: '123',
+};
