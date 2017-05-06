3f2ce4be43f766ae56c54efdd9aba2a33b084c06
Matthew Ess <mess@purdue.edu>
2017-05-06 11:52:38 -0400

    Revert "Add example config"
    This reverts commit d7f6fcc1512c6464cf6e8bed534cb1546f85a3f4.

--- js/example.config.js
+++
@@ -1,8 +0,0 @@
-let config = {
-  apiKey: 'XYZ',
-  authDomain: 'myapp.firebaseapp.com',
-  databaseURL: 'https://myapp.firebaseio.com',
-  projectId: 'myapp',
-  storageBucket: 'myapp.appspot.com',
-  messagingSenderId: '123',
-};
