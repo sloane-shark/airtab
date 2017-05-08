e57066f150f182693d163185dc8c4306a0356fc0
Matthew Ess <mess@purdue.edu>
2017-05-08 09:07:53 -0400

    Add very basic auth

--- js/background.js
+++ js/background.js
@@ -15,7 +15,24 @@ function generateTabList() {
 
 // initialize Firebase and attempt silent authentication
 function init() {
+  // initialize Firebase with configuration (config.js)
   firebase.initializeApp(config);
+  // get Chrome identity
+  chrome.identity.getAuthToken({ interactive: false }, (token) => {
+    if (chrome.runtime.lastError) {
+      // error
+    } else if (token) {
+      const credential = firebase.auth.GoogleAuthProvider.credential(null, token);
+      firebase.auth.signInWithCredential(credential).catch((error) => {
+        if (error.code === 'auth/invalid-credential') {
+          chrome.identity.removeCachedToken({ token });
+        }
+        // error
+      });
+    } else {
+      // error
+    }
+  });
 }
 
 window.onload = () => {
