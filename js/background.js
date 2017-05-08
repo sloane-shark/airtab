5a33735efdd5b384f3d27b235da9ad9de623db6e
Matthew Ess <mess@purdue.edu>
2017-05-08 08:33:13 -0400

    Start authentication from scratch with Firebase

--- js/background.js
+++ js/background.js
@@ -1,14 +1,11 @@
 // background scripting for Airtab
 
-// TODO: move this to an object probably
-let authToken;
-
-// function to add list to registry
+// add a tab list to registry
 function addList(list) {
   console.log(list);
 }
 
-// function to generate list of tabs
+// generate list of tabs
 function generateTabList() {
   chrome.tabs.query({ pinned: false }, (tabs) => {
     const list = tabs.map(tab => ({ url: tab.url, title: tab.title, favicon: tab.favIconUrl }));
@@ -16,35 +13,11 @@ function generateTabList() {
   });
 }
 
-// auth callback
-function authCallback(token) {
-  if (chrome.runtime.lastError) {
-    chrome.browserAction.setBadgeText({ text: '!' });
-    chrome.browserAction.setBadgeBackgroundColor({ color: '#F00' });
-  } else {
-    authToken = token;
-    chrome.browserAction.onClicked.addListener(generateTabList);
-  }
+// initialize Firebase and attempt silent authentication
+function init() {
+  firebase.initializeApp(config);
 }
 
-// auth function
-function auth(options = { interactive: false }) {
-  chrome.browserAction.setBadgeText({ text: '' });
-  chrome.identity.getAuthToken({ interactive: options.interactive }, authCallback);
-}
-
-// silent auth
-function authSilent() {
-  auth({ interactive: false });
-}
-
-// interactive auth
-function authInteractive() {
-  auth({ interactive: true });
-}
-
-// handler for browser button clicks
-chrome.browserAction.onClicked.addListener(authInteractive);
-
-// attempt to authorize
-authSilent();
+window.onload = () => {
+  init();
+};
